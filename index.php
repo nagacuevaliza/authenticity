55aac1abf32abe2346f541aaad86b45413d09960
lizanagacueva <108812907+nagacuevaliza@users.noreply.github.com>
2023-01-18 23:43:44 +0300

    start page & links

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="media.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Mulish&display=swap" rel="stylesheet">
+    <!-- <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">    -->
+    <title>AUTHENTICITY</title>
+  </head>
+  <body>
+    <header class="header">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <div class="header__info">
+              <h1 class="title">Проверка справок на подлинность</h1>
+              <p class="suptitle">
+                Вводный текст__________________________________________________________________________
+                ________________________________________________________________________
+                 </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+
+    <section class="about">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <h2 class="about__title">Кнопки</h2>
+
+            <!-- Row 1 -->
+            <div class="row">
+              
+            </div>
+
+            <!-- Row 2 -->
+            <div class="row">
+              
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- LINK -->
+
+    <section class="link_section">
+        <div class="container">
+            <div class="row">
+                <div class="col text-center">
+                    <h1 class="link_title">Использованный набор открытых данных доступен по ссылке:</h1>
+                </div>
+            </div>
+            <div class="database_link">
+                <a href="https://data.mos.ru/opendata/7707089084-perechen-organizatsiy-imeyushchih-litsenziyu-na-vydachu-spravok-dlya-polucheniya-voditelskih-udostovereniy-noshenie-orujiya-i-drugie?pageNumber=4&versionNumber=3&releaseNumber=21"
+                target="_blank">Перечень организаций, имеющих лицензию на выдачу справок для получения 
+                водительских удостоверений, ношение оружия и другие</a>
+            </div>
+            <!-- <div class="row">
+                 <!-- LINK -->
+            </div>
+            
+        </div>
+</section>
+
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
